895f2365131f1f6533974c6b5c3d64e95eeba717
Lucas Corsi <48248470+LucasCorsi1@users.noreply.github.com>
2020-01-17 00:31:55 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const app = express();
 
 
-mongoose.connect('mongodb+srv://sa:sa@omnistack-pmm9s.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<usuario>:<suasenha>@omnistack-pmm9s.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
